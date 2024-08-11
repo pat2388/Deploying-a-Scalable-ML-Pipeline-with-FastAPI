a05057fbe763ce54a579ee6c6c0a19371e3ab5b1
pat2388 <ptiern1@wgu.edu>
2024-08-11 18:21:19 -0400

    comments adjusted for local_api

--- local_api.py
+++ local_api.py
@@ -2,12 +2,12 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
+# send a GET using the URL http://127.0.0.1:8000
 r = requests.get("http://127.0.0.1:8000")
 
-# TODO: print the status code
+# print the status code
 print(r.status_code)
-# TODO: print the welcome message
+#  print the welcome message
 print(r.json())
 
 
@@ -29,10 +29,10 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
+# send a POST using the data above
 r = requests.post("http://127.0.0.1:8000/data", json=data)
 
-# TODO: print the status code
+# print the status code
 print(r.status_code)
-# TODO: print the result
+#  print the result
 print(r.json())
